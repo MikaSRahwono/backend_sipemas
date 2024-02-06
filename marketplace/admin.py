bb60d6ab5d88774a85097a9244df026ea38b635e
MikaSRahwono <mikasuryof@outlook.com>
2024-02-06 23:17:07 +0700

    [FIX] [SIM-41] Adding Course Information to Django Admin

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.register(Course, Topic, Field, TopicInformation, User)(admin.ModelAdmin)
+admin.register(Course, Topic, Field, TopicInformation, User, CourseInformation)(admin.ModelAdmin)
