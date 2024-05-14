7aaea4f8bf7713be7659a9055710e4451b3f6c91
Mika Suryofakhri Rahwono <mikasuryof@outlook.com>
2024-05-15 00:06:34 +0700

    [FIX] Fixing Dockerfile

--- backend_sipemas/settings/stage.py
+++ backend_sipemas/settings/stage.py
@@ -1,10 +1,29 @@
 from .base import *
+import os
+import datetime
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = [""]
 
-import datetime
-JWT_AUTH = {
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3000),
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+CORS_ALLOW_ALL_ORIGINS = True
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=120),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=7),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('DJANGO_PORT'),
+    }
 }
