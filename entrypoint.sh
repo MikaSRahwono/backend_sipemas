20478fdb6bc13860271e2d20360674bb3d3be1df
Mika Suryofakhri Rahwono <mikasuryof@outlook.com>
2024-05-15 00:11:36 +0700

    [FIX] Fixing Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+# Wait for the database to be ready
+while ! pg_isready -h $DB_HOST -p $POSTGRES_PORT -U $POSTGRES_USER; do
+  echo "Waiting for database..."
+  sleep 2
+done
+
 echo "Running Database Migrations"
-python manage.py makemigrations
-python manage.py migrate
+python manage.py makemigrations --settings=backend_sipemas.settings.stage
+
+python manage.py migrate --settings=backend_sipemas.settings.stage
+
+python manage.py loaddata api/user/fixtures/organization.json --settings=backend_sipemas.settings.stage
+
+python manage.py seed_groups --settings=backend_sipemas.settings.stage
 
 echo "Running app1 management commands"
 python manage.py sample_management_command
