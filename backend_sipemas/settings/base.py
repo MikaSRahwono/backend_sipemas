cdbb6232218ea9aea7e6e60c1e757ee2b63d14b0
Mika Suryofakhri Rahwono <mikasuryof@outlook.com>
2024-05-26 03:19:16 +0700

    [FIX] Fixing settings

--- backend_sipemas/settings/base.py
+++ backend_sipemas/settings/base.py
@@ -81,9 +81,6 @@ TEMPLATES = [
     },
 ]
 
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
-
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
